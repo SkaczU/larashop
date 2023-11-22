c5e474f74fe2e77d3696107579438fef207d87bb
SkaczU <dawid.kaczmarek@wodr.poznan.pl>
2023-11-22 18:32:14 +0100

    stworzono logowanie i rejestracje (recznie)

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,56 +1,61 @@
-//app\Http\Controllers\AuthController.php
-<?php
- 
-namespace App\Http\Controllers;
- 
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
- 
-class AuthController extends Controller
-{
-    public function register()
-    {
-        return view('register');
-    }
- 
-    public function registerPost(Request $request)
-    {
-        $user = new User();
- 
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = Hash::make($request->password);
- 
-        $user->save();
- 
-        return back()->with('success', 'Register successfully');
-    }
- 
-    public function login()
-    {
-        return view('login');
-    }
- 
-    public function loginPost(Request $request)
-    {
-        $credetials = [
-            'email' => $request->email,
-            'password' => $request->password,
-        ];
- 
-        if (Auth::attempt($credetials)) {
-            return redirect('/home')->with('success', 'Login Success');
-        }
- 
-        return back()->with('error', 'Error Email or Password');
-    }
- 
-    public function logout()
-    {
-        Auth::logout();
- 
-        return redirect()->route('login');
-    }
-}
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel 10 Custom Login and Registration - Login Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="row justify-content-center mt-5">
+        <div class="col-lg-4">
+            <div class="card text-center">
+                <div class="card-header">
+                    <h1 class="card-title">Login</h1>
+                </div>
+                <div class="card-body">
+                    @if(Session::has('error'))
+                        <div class="alert alert-danger" role="alert">
+                            {{ Session::get('error') }}
+                        </div>
+                    @endif
+                    <form action="{{ route('login') }}" method="POST">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="email" class="card-title">Email address</label>
+                            <input type="email" name="email" class="form-control" id="email" placeholder="przykladowy@mail.com" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="card-title">Password</label>
+                            <input type="password" name="password" class="form-control" id="password" required>
+                        </div>
+                        <div id="password-strength"></div>
+                        <div class="mb-3">
+                                <label for="password" class="card-title">Jeszcze raz</label>
+                                <input type="password" name="password" class="form-control" id="password" required>
+                            </div>
+                        <div class="mb-3">
+                            <div class="d-grid">
+                                <button class="btn btn-primary">Login</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/zxcvbn/4.4.2/zxcvbn.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('#password').on('input', function () {
+                var password = $(this).val();
+                var result = zxcvbn(password);
+
+                $('#password-strength').html('Strength Score: ' + result.score + '<br>Feedback: ' + result.feedback.suggestions.join(', '));
+            });
+        });
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controlers\AuthController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 
 /*
