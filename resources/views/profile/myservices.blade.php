41e051cff9a12bcc950de834cf040e4e4b7a4ab7
SkaczU <dawid.kaczmarek@wodr.poznan.pl>
2023-11-25 15:51:55 +0100

    Dodano wyświetlanie zakupionych usług ich podglad oraz walidacje

--- resources/views/profile/myservices.blade.php
+++ resources/views/profile/myservices.blade.php
@@ -1,41 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        @if($order->order_items->isNotEmpty())
-        <div class="d-flex align-items-center justify-content-between">
-            <h1 class="mb-0">Twoje usługi</h1>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <h1 class="mb-0">Dostępne Usługi</h1>
+                </div>
+                <hr />
+                <div class="card-body">
+                    @if($services->isEmpty())
+                        <p>Brak Zakupionych usług.</p>
+                    @else
+                        <table class="table table-hover">
+                            <thead class="table-primary">
+                                <tr>
+                                    <th>ID usługi</th>
+                                    <th>Nazwa Usługi</th>
+                                    <th>Usługa Aktywna od</th>
+                                    <th>Usługa Aktywna do</th>
+                                    <th>Przejdz do</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($services as $service)
+                                    <tr>
+                                        <td class="align-middle">{{ $service->service_id }}</td>
+                                        <td class="align-middle">{{ $service->service->name }}</td>
+                                        <td class="align-middle">{{ $service->start_date }}</td>
+                                        <td class="align-middle">{{ $service->end_date }}</td>
+                                        <td>
+                                            <div class="btn-group" role="group" aria-label="Basic example">
+                                                <a href="/services/{{$service->service_id}}" type="button" class="btn btn-secondary">Podgląd</a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    @endif
+                </div>
+            </div>
         </div>
-        <hr />
-
-            <table class="table table-hover">
-                <thead class="table-primary">
-                    <tr>
-                        <th>#</th>
-                        <th>Usługa</th>
-                        <th>Data Rozpoczecia</th>
-                        <th>Data Zakończenia</th>
-                        <th>Przejdz do</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($order->order_items as $services)
-                        <tr>
-                            <td class="align-middle">{{ $loop->iteration }}</td>
-                            <td class="align-middle">{{ $services->service->name }}</td>
-                            <td class="align-middle">{{ $services->start_date }}</td> 
-                            <td class="align-middle">{{ $services->end_date }}</td>
-                            <td class="align-middle">
-                                <div class="btn-group" role="group" aria-label="Basic example">
-                                    <a href="/services/{{$services  ->id}}" type="button" class="btn btn-secondary">Podglad</a>
-                                </div>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        @else
-            <p>Brak zamówionych usług.</p>
-        @endif
     </div>
+</div>
 @endsection
--- resources/views/services/2.blade.php
+++ resources/views/services/2.blade.php
@@ -8,14 +8,14 @@
         <div class="d-flex align-items-center justify-content-between">
             <h1 class="mb-0">{{$service->name}}</h1>
         </div>
-        <div class="row justify-content-center">
-            @php
-                $endDates = $orderItems->pluck('end_date')->toArray();
-                $maxEndDate = max($endDates);
-            @endphp
+            <div class="row justify-content-center">
+                <p>Skoro tu dotarłeś musisz umrzeć</p>
+            <img src="https://i.ytimg.com/vi/tXi_Bqb3q94/hqdefault.jpg" alt="Description of the image">
+            </div>
 
-            <h2 class="mb-0">Ważna do {{$maxEndDate}}</h2>
-        </div>
-    </div>
         @endif
+
+
+
+    </div>
 @endsection
